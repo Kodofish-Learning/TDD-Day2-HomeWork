aaeda59101c9cc106335cab453205aa95f2b181f
kodofish <kodofish@gmail.com>
2016-07-12 01:44:27 +0800

    完成符合第一個測試案例的Production Code

---
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterShoppingCart
+{
+    public class PotterShoppingCart
+    {
+        private List<Book> _ShoppingItems;
+
+        public void AddShoppingItem(List<Book> books)
+        {
+            this._ShoppingItems = books;
+        }
+
+        public decimal getTotalPrice()
+        {
+            return 100;
+        }
+    }
+}
