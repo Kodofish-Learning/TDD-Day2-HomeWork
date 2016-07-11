8bf2f0dbdaeaba9c7147c272a3b755dbfd34c46e
kodofish <kodofish@gmail.com>
2016-07-12 02:12:13 +0800

    完成符合4個測試案例的Production Code

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -17,11 +17,20 @@ namespace PotterShoppingCart
 
         public decimal getTotalPrice()
         {
-            decimal rate = 1;
+            decimal rate = getRate(_ShoppingItems.Count);
 
-            decimal totalPrice = (rate - (decimal)((_ShoppingItems.Count - 1) * 0.05)) * _ShoppingItems.Sum(x => x.Price);
+            decimal totalPrice = rate * _ShoppingItems.Sum(x => x.Price);
 
             return totalPrice;
+        }
+
+        private decimal getRate(int count)
+        {
+            decimal rate = 1;
+            if (count <= 3)
+                return (rate - (decimal)((_ShoppingItems.Count - 1) * 0.05));
+            else
+                return rate - (decimal)((_ShoppingItems.Count) * 0.05);
         }
     }
 }
